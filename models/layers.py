fdf90c32ec3ba7547f7e874e4eda69765a7b6fb7
aneesh-aparajit <aneeshaparajit.g2002@gmail.com>
2023-02-20 08:45:55 +0530

    implemented conv-lstm

--- models/layers.py
+++ models/layers.py
@@ -2,7 +2,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
+from typing import Tuple, Optional
 
 # %%
 class ConvLayer(nn.Module):
@@ -24,14 +24,13 @@ class ConvLayer(nn.Module):
             padding=padding
         )
 
-        self.bn = nn.BatchNorm2d(num_features=out_channels)
+        self.bn = nn.BatchNorm3d(num_features=out_channels)
 
         self.relu = nn.ReLU()
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.relu(self.bn(self.conv(x)))
         return x
-
 
 # %%
 class EncoderLayer(nn.Module):
@@ -42,10 +41,10 @@ class EncoderLayer(nn.Module):
             *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         
-        self.conv1 = ConvLayer(in_channels=in_channels, out_channels=32, kernel_size=11, stride=4, padding=1)
-        self.conv2 = ConvLayer(in_channels=32, out_channels=16, kernel_size=9, stride=4, padding=1)
-        self.conv3 = ConvLayer(in_channels=16, out_channels=1, kernel_size=4, stride=1, padding=1)
-        self.fc    = nn.Linear(in_features=121, out_features=latent_size)
+        self.conv1 = ConvLayer(in_channels=in_channels, out_channels=32, kernel_size=8, stride=4, padding=1)
+        self.conv2 = ConvLayer(in_channels=32, out_channels=16, kernel_size=2, stride=4, padding=1)
+        self.conv3 = ConvLayer(in_channels=16, out_channels=1, kernel_size=2, stride=1, padding=1)
+        self.fc    = nn.Linear(in_features=162, out_features=latent_size)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         batch_size = x.size(0)
@@ -56,26 +55,45 @@ class EncoderLayer(nn.Module):
         x = self.fc(x)
         return x
 
-x = torch.randn((5, 3, 224, 224))
-y = EncoderLayer(in_channels=3, latent_size=100).forward(x)
-
-
 # %%
 class Pix2PixRNN(nn.Module):
     def __init__(
             self, 
             in_channels: int = 3, 
             latent_size: int = 128,
+            hidden_size: int = 512,
+            num_layers: int = 2,
+            bidirectional: Optional[int] = 1,
             *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
+
+        self.in_channels = in_channels
+        self.latent_size = latent_size
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.bidirectional = bidirectional
+
         self.encoder = EncoderLayer(in_channels=in_channels, latent_size=latent_size)
+        self.lstm = nn.LSTM(
+            input_size=latent_size,
+            hidden_size=hidden_size, 
+            num_layers=num_layers, 
+            batch_first=True,
+            bidirectional=True if self.bidirectional == 2 else False
+        )
+
+    def _init_hidden_states(self, bs: int) -> Tuple[torch.Tensor]:
+        return torch.zeros(self.bidirectional*self.num_layers, bs, self.hidden_size), torch.zeros(self.bidirectional*self.num_layers, bs, self.hidden_size)
     
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return self.encoder(x)
+    def forward(self, x: torch.Tensor) -> Tuple[torch.Tensor]:
+        # x = self.encoder(x)
+        h0, c0 = self._init_hidden_states(64)
+        out, (h, c) = self.lstm(x, (h0, c0))
+        return out, h, c
 
 
-x = torch.randn((32, 8, 3, 224, 224))
-model = Pix2PixRNN(in_channels=3, latent_size=128)
-y = model.forward(x)
-y.shape
+x = torch.randn((64, 8, 128))
+model = Pix2PixRNN(in_channels=3, latent_size=128, bidirectional=2)
+out, h, c = model.forward(x)
+out.shape, h.shape, c.shape
 # %%
