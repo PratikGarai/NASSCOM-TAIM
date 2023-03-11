ed27a884123adba7559c7e7dd6f2e62b6610b220
aneesh-aparajit <aneeshaparajit.g2002@gmail.com>
2023-03-11 23:43:15 +0530

    configured project and setup

--- pix2pix_rnn/setup.py
+++ pix2pix_rnn/setup.py
@@ -1,1 +1,24 @@
 # setup.py
+from pathlib import Path
+from setuptools import find_namespace_packages, setup
+
+
+BASE_DIR = Path(__file__).parent
+with open(Path(BASE_DIR, "requirements.txt"), "r") as f:
+    required_installs = [ln.strip() for ln in f.readlines()]
+
+
+setup(
+    name="weatherai",
+    version="0.0.3",
+    author=[
+        "Aneesh Aparajit G", 
+        "Pratik Garai", 
+        "Neeraj J Manurkar",
+        "Aravind P"
+    ], 
+    install_requires=[required_installs],
+    packages=find_namespace_packages(),
+    description="""This is a package which has Pix2Pix models built on PyTorch for predicting the weather of states based on visual interpretation of the tabular data.""",
+    keywords=["weather", "ai", "pytorch", "modelling", "hackathon", "nasscom"]
+)
--- pix2pix_rnn/src/config/config.py
+++ pix2pix_rnn/src/config/config.py
@@ -1,1 +1,46 @@
 # config/config.py
+import torch
+from pathlib import Path
+import os
+
+
+class config:
+    seed          = 42
+    exp_name      = 'nasscom-taim'
+    model_name    = 'pix2pix-rnn'
+    base_model    = 'None'
+    train_bs      = 32
+    valid_bs      = 2 * train_bs
+    image_size    = [224, 224]
+    in_channels   = 3
+    latent_size   = 128
+    hidden_size   = 512
+    num_layers    = 2
+    bidirectional = 0 # could be 0 or 1
+    comment       = f'model-{model_name}|latent_size-{latent_size}|info_channels-{in_channels}|hidden_dim-{hidden_size}|num_layers-{num_layers}|bidirectional-{bidirectional}|dim-{image_size[0]}x{image_size[1]}'
+    epochs        = 10
+    learning_rate = 3e-4
+    scheduler     = 'CosineAnnealingLR'
+    min_lr        = 1e-6
+    T_max         = int(30000/train_bs*epochs)+50
+    T_0           = 25
+    warmup_epochs = 0
+    wd            = 1e-6
+    n_accumulate  = max(1, 32//train_bs)
+    n_fold        = 5
+    num_classes   = 3
+    device        = torch.device("cuda:0" if torch.cuda.is_available() else "mps" if torch.has_mps else "cpu")
+    train_num     = 1
+
+
+if __name__ == '__main__':
+    BASE_DIR = Path(__file__).parent.parent.absolute()
+    CONFIG_DIR = Path(BASE_DIR, "config")
+    DATA_DIR = Path(BASE_DIR, "data")
+    MODEL_DIR = Path(BASE_DIR, "models")
+
+    if not os.path.exists(DATA_DIR):
+        DATA_DIR.mkdir(parents=True, exist_ok=True)
+    
+    if not os.path.exists(MODEL_DIR):
+        MODEL_DIR.mkdir(parents=True, exist_ok=True)
