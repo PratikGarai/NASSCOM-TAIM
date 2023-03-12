5860aee950d42ab7348b93868a1e2a9651160934
aneesh-aparajit <aneeshaparajit.g2002@gmail.com>
2023-03-12 09:03:06 +0530

    implemented basic code

--- pix2pix_rnn/src/weatherai/model.py
+++ pix2pix_rnn/src/weatherai/model.py
@@ -1,1 +1,4 @@
 # weatherai/model.py
+import torch
+import torch.nn as nn
+from typing import Optional, List
--- pix2pix_rnn/src/weatherai/utils/data.py
+++ pix2pix_rnn/src/weatherai/utils/data.py
@@ -1,1 +1,50 @@
 # weatherai/utils/data.py
+import torch
+import torch.optim as optim
+from torch.optim import lr_scheduler
+from config import config
+import numpy as np
+import random
+import os
+
+
+def get_optimizer(optimizer: optim):
+    if config.scheduler == 'CosineAnnealingLR':
+        scheduler = lr_scheduler.CosineAnnealingLR(
+            optimizer=optimizer, 
+            T_max=config.T_max, 
+            eta_min=config.min_lr
+        )
+    elif config.scheduler == 'CosineAnnealingWarmRestarts':
+        scheduler = lr_scheduler.CosineAnnealingWarmRestarts(
+            optimizer=optimizer, 
+            T_0=config.T_0, 
+            eta_min=config.eta_min
+        )
+    elif config.scheduler == 'ReduceLROnPlateau':
+        scheduler = lr_scheduler.ReduceLROnPlateau(
+            optimizer=optimizer, 
+            mode='min',
+            factor=0.1, 
+            patience=10, 
+            threshold=0.0001, 
+            min_lr=config.min_lr
+        )
+    elif config.scheduler == 'ExponentialLR':
+        scheduler = lr_scheduler.ExponentialLR(
+            optimizer=optimizer, 
+            gamma=0.85
+        )
+    elif config.scheduler is None:
+        scheduler = None
+    else:
+        raise NotImplementedError("The Scheduler you have asked has not been implemented")
+    return scheduler
+
+
+def set_seed(seed: int = 42):
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.manual_seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    print('>>> SEEDED <<<')
